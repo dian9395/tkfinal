b67da7feb846f9f8b55329742fee74d6297707eb
dian9395 <dian9395@gmai.com>
2019-05-20 12:24:56 +0700

    add SQL

---
+++ application/views/SQL/ACARA.sql
@@ -0,0 +1,13 @@
+INSERT INTO ACARA VALUES 
+('4638997388','Donor Darah PMI','Donor Darah','2018/2/2','2018/2/3',TRUE),
+('7776022540','Bazar Pakaian','Jual Pakaian','2018/6/27','2018/7/7',TRUE),
+('380830473','Talk Show','Diskusi Artis','2018/11/29','2018/11/30',FALSE),
+('7450506765','Senam Bersama','Senam','2018/11/7','2018/11/7',TRUE),
+('2526896136','Sepeda Gembira','Pergi Bersepeda','2018/8/5','2018/8/5',TRUE),
+('8806086329','Kemerdekaan RI','Lomba dan event','2018/8/17','2018/8/18',TRUE),
+('3343942311','Lomba Balap Sepeda','Lomba Sepeda','2018/12/3','2018/12/4',FALSE),
+('2593736907','Bazar Makanan Murah','Jual Makanan','2018/7/20','2018/7/22',TRUE),
+('2019536366','Bazar Minuman Murah','Jual Minuman','2018/11/3','2018/11/5',FALSE),
+('9199887463','Jalan Santai','Jalan Bersama','2018/9/22','2018/9/22',TRUE);
+
+
