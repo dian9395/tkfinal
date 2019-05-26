7b1e4dda5fb69b447fa12b533cf9fdccd66a82ad
dian9395 <dian9395@gmail.com>
2019-05-26 14:51:59 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -36,7 +36,7 @@ $config['base_url'] = 'https://github.com/dian9395/tkfinal';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
