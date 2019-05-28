81410b17ee1d67d2aad162823be8a0420651b2d3
dian9395 <dian9395@gmail.com>
2019-05-28 15:09:57 +0700

    update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -27,11 +27,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 //$config['base_url'] = 'https://basdatk5.herokuapp.com/';
 
-$root=(isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
-$root.= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
-$config['base_url'] = $root;
+//$root=(isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
+//$root.= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+//$config['base_url'] = $root;
 
-//$config['base_url']   = 'http://localhost/dbs/';
+$config['base_url']   = 'http://localhost/dbs/';
 
 /*
 |--------------------------------------------------------------------------
