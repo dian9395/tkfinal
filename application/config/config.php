c0d3e941d88196ae626e9a89199a8084a20999a3
Dian <dian9395@gmail.com>
2019-05-28 17:20:48 +0700

    update urldb, config.php

--- application/config/config.php
+++ application/config/config.php
@@ -31,7 +31,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 //$root.= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
 //$config['base_url'] = $root;
 
-$config['base_url']   = 'http://localhost/dbs/';
+$config['base_url']   = 'http://localhost/tkfinal/';
 
 /*
 |--------------------------------------------------------------------------
